fb392f9a13e085674cda00a1bb9f1dad1b9449bd
Glenn West <gwest@redhat.com>
2017-09-25 01:48:47 +0000

    Add sleep before looking at logs

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ sleep 20
 oc new-project mssql
 oc new-app https://github.com/glennswest/mssqlrhel 
 oc volume "dc/mssqlrhel" --add --mount-path=/var/opt/mssql --type=persistentVolumeClaim --claim-name=mssqlrhel --claim-mode="ReadWriteOnce" --claim-size=1G
+sleep 20
 oc logs -f bc/mssqlrhel
 
