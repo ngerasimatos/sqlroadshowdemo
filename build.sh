a45e99262a491c42c00cee8db2fcb23bfa1cbdc9
Steven Ellis <steven.ellis@gmail.com>
2018-02-16 10:14:29 +1300

    Clean up the build script to include comments and only clean up project if currently present

--- build.sh
+++ build.sh
@@ -1,10 +1,22 @@
-oc delete project mssql
-sleep 60
+#!/bin/bash
+#
+# Clean up any environment if required
+(oc get projects | grep mssql) &&  oc delete project mssql && sleep 60
+
+# Create a new project with the correct policy
 oc new-project mssql 
 oc adm policy add-scc-to-user anyuid -z default
+
+# Create our mssql demo application
 oc new-app https://github.com/ngerasimatos/sqlroadshowdemo
+
+# Inject our environment details
 oc env "dc/sqlroadshowdemo" -e ACCEPT_EULA=y -e SA_PASSWORD=RedHat123!
+
+# and connect a persistent volume
 oc volume "dc/sqlroadshowdemo" --add --mount-path=/var/opt/mssql --type=persistentVolumeClaim --claim-name=sqlroadshowdemo --claim-mode="ReadWriteOnce" --claim-size=1G
+
+# Give the environent a chance to start before tailing the logs
 sleep 20
 oc logs -f bc/sqlroadshowdemo
 
