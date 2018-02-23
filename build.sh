333507c00b62c879cd8082a30c3320acca1aa09a
ngerasimatos <nicholas.gerasimatos@gmail.com>
2018-02-23 09:07:26 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,3 @@ oc env "dc/sqlroadshowdemo" -e ACCEPT_EULA=y -e SA_PASSWORD=RedHat123!
 oc volume "dc/sqlroadshowdemo" --add --mount-path=/var/opt/mssql --type=persistentVolumeClaim --claim-name=sqlroadshowdemo --claim-mode="ReadWriteOnce" --claim-size=1G
 sleep 20
 oc logs -f bc/sqlroadshowdemo
-
